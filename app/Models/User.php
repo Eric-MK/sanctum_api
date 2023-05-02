2224c740352b78465e96f6cdf15c73b96afe6fb5
unknown <mutungaeric839@gmail.com>
2023-05-02 18:17:47 +0300

    making user model to generate api tokens

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Laravel\Sanctum\HasApiTokens;//Laravel PHP web application framework that can be used by an Eloquent model to enable the model to issue API tokens using the Sanctum authentication library.
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;//By adding the HasApiTokens trait, the model gains the ability to generate API tokens that can be used to authenticate API requests.
 
     /**
      * The attributes that are mass assignable.
